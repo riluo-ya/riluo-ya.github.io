08f6e61ee9fbb1a87905bae4ea5cbbdf5476f7f8
日落-ya <3571278927@qq.com>
2026-01-01 11:30:43 +0800

    Delete tool/idcard directory

--- tool/idcard/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {}
-	}
-}
-// eslint-disable-next-line no-undef
-module.exports = config
