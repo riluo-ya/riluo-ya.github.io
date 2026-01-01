87190fabf3db88c52f155304a02c51c713162aaa
日落-ya <3571278927@qq.com>
2026-01-01 11:22:47 +0800

    Delete tool/idcard-generator-tauri-master directory

--- tool/idcard-generator-tauri-master/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-
-export default {
-	content: ['./index.html', './splashscreen.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-	theme: {
-		extend: {}
-	},
-	plugins: []
-} satisfies Config
